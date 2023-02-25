3aa7ec7ec9d3310d717f508e6697018f53b66338
G-A-N-IMAS <spamcapourvoir@gmail.com>
2023-02-25 17:57:59 +0100

    Mise en place du style général

---
+++ src/components/UI/Layout.js
@@ -0,0 +1,23 @@
+import styled , {ThemeProvider} from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+import lightTheme from "./themes/light.json"
+import darkTheme from "./themes/dark.json"
+import { useState } from "react";
+
+export default function Layout({children}){
+    const [isLight, setIsLight] = useState(true);
+function handleToggleTheme(){
+    setIsLight(!isLight);
+}
+
+    return (
+        <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
+            <Wrapper>
+                <GlobalStyle/>{children}
+                <button onClick={handleToggleTheme}>Switch to {isLight? "dark" : "light"} theme</button>
+            </Wrapper>
+        </ThemeProvider>
+    )
+};
+
+const Wrapper = styled.div``;
